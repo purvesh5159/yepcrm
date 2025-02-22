e4789f786606ee174f775cbb09d0fb16fe08ee87
purveshramoliya <purveshr@biztechnosys.com>
2025-02-22 10:16:12 +0530

    pdf format

---
+++ test/templates_c/v7/4e41178c42b231f575942f3b0c665aedce0010e6_0.file.DetailViewPreProcess.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/* Smarty version 4.5.4, created on 2025-02-21 13:08:51
+  from 'D:\wamp\www\yepcrm\layouts\v7\modules\PDFMaker\DetailViewPreProcess.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.4',
+  'unifunc' => 'content_67b87ae31b9850_78165022',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4e41178c42b231f575942f3b0c665aedce0010e6' => 
+    array (
+      0 => 'D:\\wamp\\www\\yepcrm\\layouts\\v7\\modules\\PDFMaker\\DetailViewPreProcess.tpl',
+      1 => 1740051497,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:modules/Vtiger/partials/Topbar.tpl' => 1,
+  ),
+),false)) {
+function content_67b87ae31b9850_78165022 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:modules/Vtiger/partials/Topbar.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+<div class="container-fluid app-nav">
+    <div class="row">
+        <?php $_smarty_tpl->_subTemplateRender(call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'vtemplate_path' ][ 0 ], array( "partials/SidebarHeader.tpl",$_smarty_tpl->tpl_vars['MODULE']->value )), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+        <?php $_smarty_tpl->_subTemplateRender(call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'vtemplate_path' ][ 0 ], array( "ModuleHeader.tpl",$_smarty_tpl->tpl_vars['MODULE']->value )), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+    </div>
+</div>
+</nav>    
+     <div id='overlayPageContent' class='fade modal overlayPageContent content-area overlay-container-60' tabindex='-1' role='dialog' aria-hidden='true'>
+        <div class="data">
+        </div>
+        <div class="modal-dialog">
+        </div>
+    </div>
+<div class="container-fluid main-container">
+    <div class="row">
+        <div id="modnavigator" class="module-nav detailViewModNavigator clearfix">
+            <div class="hidden-xs hidden-sm mod-switcher-container">
+                <?php $_smarty_tpl->_subTemplateRender(call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'vtemplate_path' ][ 0 ], array( "partials/Menubar.tpl",$_smarty_tpl->tpl_vars['MODULE']->value )), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+            </div>
+        </div>
+        <div class="detailViewContainer viewContent clearfix">
+            <div class="col-sm-12 col-xs-12 content-area">
+                <?php $_smarty_tpl->_subTemplateRender(call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'vtemplate_path' ][ 0 ], array( "DetailViewHeader.tpl",$_smarty_tpl->tpl_vars['MODULE']->value )), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+            </div>                <div class="detailview-content container-fluid">
+                    <input id="recordId" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['RECORD']->value->getId();?>
+" />
+                    <?php $_smarty_tpl->_subTemplateRender(call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'vtemplate_path' ][ 0 ], array( "ModuleRelatedTabs.tpl",$_smarty_tpl->tpl_vars['MODULE']->value )), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+                    <div class="details row" style="margin-top:10px;">
+<?php }
+}
---
+++ test/templates_c/v7/dd6247371b934c9ecc1c87f7dc7cd9cbc4fbd451_0.file.ListPDFTemplatesContents.tpl.php
@@ -0,0 +1,93 @@
+<?php
+/* Smarty version 4.5.4, created on 2025-02-21 13:08:45
+  from 'D:\wamp\www\yepcrm\layouts\v7\modules\PDFMaker\ListPDFTemplatesContents.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.4',
+  'unifunc' => 'content_67b87add4a3479_42773410',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'dd6247371b934c9ecc1c87f7dc7cd9cbc4fbd451' => 
+    array (
+      0 => 'D:\\wamp\\www\\yepcrm\\layouts\\v7\\modules\\PDFMaker\\ListPDFTemplatesContents.tpl',
+      1 => 1740051497,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67b87add4a3479_42773410 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="col-sm-12 col-xs-12 ">
+
+    <input type="hidden" name="idlist" >
+    <input type="hidden" name="module" value="PDFMaker">
+    <input type="hidden" name="parenttab" value="Tools">
+    <input type="hidden" name="view" value="List">
+    <div id="table-content" class="table-container">
+
+        <form name='list' id='listedit' action='' onsubmit="return false;">
+            <table id="listview-table" class="table <?php if ($_smarty_tpl->tpl_vars['LISTVIEW_ENTRIES_COUNT']->value == '0') {?>listview-table-norecords <?php }?> listview-table">
+                <thead>
+                <tr class="listViewContentHeader">
+                    <th></th>
+                    <th nowrap="nowrap"><a href="#" data-columnname="module" data-nextsortorderval="<?php echo $_smarty_tpl->tpl_vars['module_dir']->value;?>
+" class="listViewContentHeaderValues">&nbsp;<?php echo vtranslate("LBL_MODULENAMES",$_smarty_tpl->tpl_vars['MODULE']->value);?>
+&nbsp;</a></th>
+                    <th nowrap="nowrap"><a href="#" data-columnname="description" data-nextsortorderval="<?php echo $_smarty_tpl->tpl_vars['description_dir']->value;?>
+" class="listViewContentHeaderValues"><?php echo vtranslate("LBL_DESCRIPTION",$_smarty_tpl->tpl_vars['MODULE']->value);?>
+&nbsp;</a></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['PDFTEMPLATES']->value, 'template', false, NULL, 'mailmerge', array (
+));
+$_smarty_tpl->tpl_vars['template']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['template']->value) {
+$_smarty_tpl->tpl_vars['template']->do_else = false;
+?>
+                    <tr class="listViewEntries" data-id="<?php echo $_smarty_tpl->tpl_vars['template']->value['templateid'];?>
+" data-recordurl="index.php?module=PDFMaker&view=DetailFree&templateid=<?php echo $_smarty_tpl->tpl_vars['template']->value['templateid'];?>
+" id="PDFMaker_listView_row_<?php echo $_smarty_tpl->tpl_vars['template']->value['templateid'];?>
+">
+                        <td class="listViewRecordActions">
+                            <div class="table-actions">
+                                   <span class="more dropdown action">
+                                            <span href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+                                                <i class="fa fa-ellipsis-v icon"></i></span>
+                                                <ul class="dropdown-menu">
+                                                    <li><a data-id="<?php echo $_smarty_tpl->tpl_vars['template']->value['templateid'];?>
+" href="index.php?module=PDFMaker&view=DetailFree&templateid=<?php echo $_smarty_tpl->tpl_vars['template']->value['templateid'];?>
+&app=<?php echo $_smarty_tpl->tpl_vars['SELECTED_MENU_CATEGORY']->value;?>
+"><?php echo vtranslate('LBL_DETAILS',$_smarty_tpl->tpl_vars['MODULE']->value);?>
+</a></li>
+                                                    <?php echo $_smarty_tpl->tpl_vars['template']->value['edit'];?>
+
+                                                </ul>
+                                        </span>
+                            </div>
+                        </td>
+                        <td class="listViewEntryValue"><?php echo $_smarty_tpl->tpl_vars['template']->value['module'];?>
+</a></td>
+                        <td class="listViewEntryValue"><?php echo $_smarty_tpl->tpl_vars['template']->value['description'];?>
+&nbsp;</td>
+                    </tr>
+                <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                </tbody>
+            </table>
+        </form>
+    </div>
+</div>
+<br>
+<div align="center" class="small" style="color: rgb(153, 153, 153);"><?php echo vtranslate("PDF_MAKER",$_smarty_tpl->tpl_vars['MODULE']->value);?>
+ <?php echo $_smarty_tpl->tpl_vars['VERSION']->value;?>
+ <?php echo vtranslate("COPYRIGHT",$_smarty_tpl->tpl_vars['MODULE']->value);?>
+</div><?php }
+}
